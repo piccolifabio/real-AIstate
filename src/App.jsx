ecbc8bcc31f8530683cb8bf54dd8f0a2565affc9
piccolifabio <fabiopiccoli@hotmail.it>
2026-04-27 19:24:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AffittiPage from "./AffittiPage.jsx";
 import ListingPage from "./Listing.jsx";
 import BlogPage from "./BlogPage.jsx";
 import BlogVerona from "./blog/BlogVerona.jsx";
+import BlogOMI2025 from "./blog/BlogOMI2025.jsx";
 
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&family=DM+Serif+Display:ital@0;1&display=swap');
@@ -766,6 +767,7 @@ export default function App() {
         <Route path="/compra" element={<ListingPage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/blog/agenzie-abusive-verona" element={<BlogVerona />} />
+        <Route path="/blog/mercato-immobiliare-2025-dati" element={<BlogOMI2025 />} />
       </Routes>
     </>
   );
