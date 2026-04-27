da49d0fdf0fe3fd508680be5d5482041d7ff5ebf
piccolifabio <fabiopiccoli@hotmail.it>
2026-04-27 19:40:33 +0200

    Delete src/blog/BlogRouter.jsx

--- src/blog/BlogRouter.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams, Navigate } from "react-router-dom";
-import articoli from "./index.js";
-
-export default function BlogRouter() {
-  const { slug } = useParams();
-  const Componente = articoli[slug];
-  if (!Componente) return <Navigate to="/blog" replace />;
-  return <Componente />;
-}
