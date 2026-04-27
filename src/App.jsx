c2b98068e178e33617699e252fd1a16e944b7f5f
piccolifabio <fabiopiccoli@hotmail.it>
2026-04-27 18:20:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import SiteFooter from "./SiteFooter.jsx";
 import AffittiPage from "./AffittiPage.jsx";
 import ListingPage from "./Listing.jsx";
 import BlogPage from "./BlogPage.jsx";
+import BlogVerona from "./blog/BlogVerona.jsx";
+import BlogArticoloVerona from "./BlogArticoloVerona.jsx";
 
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&family=DM+Serif+Display:ital@0;1&display=swap');
@@ -764,6 +766,8 @@ export default function App() {
         <Route path="/affitti" element={<AffittiPage />} />
         <Route path="/compra" element={<ListingPage />} />
         <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/agenzie-abusive-verona" element={<BlogVerona />} />
+        <Route path="/blog/agenzie-abusive-verona" element={<BlogArticoloVerona />} />
       </Routes>
     </>
   );
