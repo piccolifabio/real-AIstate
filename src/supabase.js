32e98e7ad200cd35dc02469921359db4344a0656
Fabio Piccoli <fabio@example.com>
2026-05-04 08:57:17 +0200

    fix: chiave Supabase corretta

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://strigywjvkhbubyszuxp.supabase.co'
-const supabaseAnonKey = 'sb_publishable_goE-x1uW6MbVteK13NgcgQ_EHgn5xbD'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0cmlneXdqdmtoYnVieXN6dXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MzM0OTcsImV4cCI6MjA5MTUwOTQ5N30.aoe0BIuDrJyYU15hWVBaxO1I8PxweWpy6assvprRkns'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
